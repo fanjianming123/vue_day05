9a1deadeec245e3d5619c529a91375535068aa64
jiuer12138 <18815283494@163.com>
2022-07-03 21:14:26 +0800

    完成todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="todoapp">
+    <!-- 除了驼峰, 还可以使用-转换链接 -->
+    <TodoHeader
+      @add="addFn"
+      :list="list"
+      @setChecked="setCheckedFn"
+    ></TodoHeader>
+    <TodoMain :list="show" @del="delFn"></TodoMain>
+    <TodoFooter
+      :count="count"
+      @clear="clearFn"
+      @toggleSel="toggleSelFn"
+    ></TodoFooter>
+  </section>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoFooter from './components/TodoFooter.vue'
+import TodoMain from './components/TodoMain.vue'
+import TodoHeader from './components/TodoHeader.vue'
 export default {
-  name: 'App',
+  data() {
+    return {
+      list: JSON.parse(localStorage.getItem('list')) || [],
+      isShow: 1,
+    }
+  },
   components: {
-    HelloWorld
-  }
+    TodoHeader,
+    TodoFooter,
+    TodoMain,
+  },
+  computed: {
+    count() {
+      return this.list.filter((ele) => !ele.isDone).length
+    },
+    show() {
+      if (this.isShow === 2) {
+        return this.list.filter((ele) => !ele.isDone)
+      } else if (this.isShow === 3) {
+        return this.list.filter((ele) => ele.isDone)
+      } else {
+        return this.list
+      }
+    },
+  },
+  methods: {
+    addFn(val) {
+      this.list.push({
+        id: this?.list[this.list.length - 1]?.id + 1 || 100,
+        name: val,
+        isDone: false,
+      })
+    },
+    setCheckedFn(val) {
+      this.list.forEach((ele) => (ele.isDone = val))
+    },
+    delFn(id) {
+      this.list = this.list.filter((ele) => ele.id !== id)
+    },
+    clearFn() {
+      this.list = this.list.filter((ele) => !ele.isDone)
+    },
+    toggleSelFn(val) {
+      this.isShow = val
+    },
+  },
+  watch: {
+    list: {
+      deep: true,
+      immediate: true,
+      handler(val) {
+        localStorage.setItem('list', JSON.stringify(val || []))
+      },
+    },
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,54 @@
+<template>
+  <footer class="footer">
+    <span class="todo-count"
+      >剩余<strong>{{ count }}</strong></span
+    >
+    <ul class="filters">
+      <li>
+        <a
+          :class="{ selected: isSel === 1 }"
+          href="javascript:;"
+          @click="change(1)"
+          >全部</a
+        >
+      </li>
+      <li>
+        <a
+          :class="{ selected: isSel === 2 }"
+          href="javascript:;"
+          @click="change(2)"
+          >未完成</a
+        >
+      </li>
+      <li>
+        <a
+          :class="{ selected: isSel === 3 }"
+          href="javascript:;"
+          @click="change(3)"
+          >已完成</a
+        >
+      </li>
+    </ul>
+    <button class="clear-completed" @click="clear">清除已完成</button>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isSel: 1,
+    }
+  },
+  props: ['count'],
+  methods: {
+    clear() {
+      this.$emit('clear')
+    },
+    change(val) {
+      this.isSel = val
+      this.$emit('toggleSel',val)
+    },
+  },
+}
+</script>
